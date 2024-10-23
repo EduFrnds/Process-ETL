24968e16df4c0a72d2f4d513eb1a54e5318180e3
Eduardo <edufernandes.contato@gmail.com>
2024-10-23 16:39:09 -0300

    commit 014 - Inclusão do arquivo upload.py

---
+++ etl/layer_silver/upload.py
@@ -0,0 +1,30 @@
+from etl.data_manager import BaseLoader
+
+
+class UploadToSilver(BaseLoader):
+    def __init__(self):
+        super().__init__()
+
+    def insert_layer_silver(self, row):
+        schema = {
+            'equipment_id': int,
+            'production': float,
+            'maintenance_type': str,
+            'month': int,
+            'year': int,
+            'maintenance_minutes': float,
+            'production_minutes': float,
+            'temperature_mean': float,
+            'vibration_level_mean': float,
+            'temperature_std': float,
+            'vibration_level_std': float,
+            'temperature_lsc': float,
+            'temperature_lsi': float,
+            'vibration_level_lsc': float,
+            'vibration_level_lsi': float
+        }
+        values = self.process_row(row, schema)
+        columns = list(schema.keys())
+        self.insert_data('layer_silver.silver_data', columns, values)
+
+
