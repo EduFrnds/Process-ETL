e204479f2c5ca88911cb761ed323beab9bd78e22
Eduardo <edufernandes.contato@gmail.com>
2024-11-11 15:12:42 -0300

    commit 002 - Teste camada Bronze, upload e inserção no banco de dados.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,29 +1,42 @@
+import csv
+
 import pytest
 from unittest.mock import patch
+
+from etl.generate_data import EquipmentProductionDataGenerator
 
 
 @pytest.fixture
 def mock_data_bronze():
-    PATCH_MOCK = 'etl.layer_bronze.upload.UploadToBronze.insert_layer_bronze'
+    """
+    Mock para os dados da camada bronze.
+    """
+    file_path = './data_test/equipments.csv'
+    data = []
 
-    with patch(PATCH_MOCK) as mock:
-        mock.return_value = {
-            'equipment_id': 1,
-            'production': 100.5,
-            'hours_production': '2023-10-31 08:30:00',
-            'temperature': 250.5,
-            'pressure': 150.0,
-            'speed': 75.2,
-            'vibration_level': 1.5,
-            'operation_status': 'ON',
-            'maintenance_type': 'Repair',
-            'hours_maintenance': 3.5
-        }
-        yield mock
+    with open(file_path, newline='', encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            data.append(row)
+
+    return data
+
+
+@pytest.fixture
+def equipment_generator(tmp_path):
+    """
+    Fixture para inicializar o gerador de dados.
+    """
+    generator = EquipmentProductionDataGenerator()
+    generator.data_manager.output_dir = tmp_path
+    return generator
 
 
 @pytest.fixture
 def db_config_mock():
+    """
+    Mock para a configuração do banco de dados.
+    """
     return {
         "dbname": "process_etl",
         "user": "seu_usuario",
