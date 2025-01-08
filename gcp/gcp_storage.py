2df0706fd5e0928ab829e859b4c9ae112b5b012f
Eduardo <edufernandes.contato@gmail.com>
2025-01-08 15:30:52 -0300

    commit cloud-002 - [skip ci] iniciando o cloud.

--- gcp/gcp_storage.py
+++ gcp/gcp_storage.py
@@ -1,21 +1,20 @@
-import logging
-
-from google.cloud import storage
-
-
-class ManagerGCP:
-
-    def __init__(self):
-        self.storage_client = storage.Client()
-
-    def upload_to_gcs(self, bucket_name, file, file_name, content_type):
-        """Uploads a file to the bucket."""
-        try:
-            bucket = self.storage_client.bucket(bucket_name)
-            blob = bucket.blob(file_name)
-            blob.upload_from_file(file, content_type=content_type)
-            # blob.make_public()
-
-        except Exception:
-            logging.error(f'GCPFactory - Error checking permission: {file_name}')
-            raise
+# import logging
+#
+# from google.cloud import storage
+#
+#
+# class ManagerGCP:
+#
+#     def __init__(self):
+#         self.storage_client = storage.Client()
+#
+#     def upload_to_gcs(self, bucket_name, file, file_name, content_type):
+#         try:
+#             bucket = self.storage_client.bucket(bucket_name)
+#             blob = bucket.blob(file_name)
+#             blob.upload_from_file(file, content_type=content_type)
+#             # blob.make_public()
+#
+#         except Exception:
+#             logging.error(f'GCPFactory - Error checking permission: {file_name}')
+#             raise
